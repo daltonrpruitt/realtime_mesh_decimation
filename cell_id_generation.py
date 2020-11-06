f473601f8e64f86a507407cfd7142c0255575e2c
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2020-11-06 09:14:14 -0600

    Add visualization to testing the cell indicies and id generation
    Using matplot lib and two tris. Based off of https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html

--- cell_id_generation.py
+++ cell_id_generation.py
@@ -48,72 +48,33 @@ def get_vertex_cell_indices_ids(vertices = None, resolution=None):
 
     return vertex_cluster_cell_indices, vertex_cluster_cell_ids
 
-'''
-resolution = 10
-obj_mesh = pywavefront.Wavefront("meshes/ssbb-toon-link-obj/DolToonlinkR1_fixed.obj", collect_faces=True)
-vertices = np.array(obj_mesh.vertices, dtype='f4')
-bbox = utility.bounding_box(vertices) # Makes bounding box
 
-# Pass 1 - Step 1 : Determine Cell ID
-#      a. Shrink model to -1, 1
-avg = [(bbox[0][i] + bbox[1][i]) / 2.0 for i in range(3) ]
-scale = [(bbox[1][i] - bbox[0][i]) for i in range(3) ] # / 2.0
-mesh_min = bbox[0]
-print("Min =", mesh_min)
-print("Avg =", avg, "  Scale =",scale)
-pos_verts = []
-for vert in vertices:
-    pos_verts.append([vert[i] + -mesh_min[i] for i in range(3)])
-x, y, z = zip(*pos_verts)
-print("Min =", min(x), min(y), min(z))
-print("Max =", max(x), max(y), max(z))
-print("Scale = ", scale)
+def test_cluster_cell_id_gen():
+    from mpl_toolkits import mplot3d
+    import matplotlib.pyplot as plt
+    verts = [
+        [0.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [0.5, 1.0, 0.0],
+        [1.0, 1.0, -1.0],
+        ]
+    indices, ids = get_vertex_cell_indices_ids(vertices=verts,resolution=3)
+    print(indices)
+    print(ids)
 
-scaled_verts = []
-for vert in pos_verts:
-    scaled_verts.append([vert[i] * resolution**2 / (scale[i]+0.00001) for i in range(3)])
+    xdata = [v[0] for v in verts]
+    ydata = [v[1] for v in verts]
+    zdata = [v[2] for v in verts]
 
-x, y, z = zip(*scaled_verts)
-print("Min =", min(x), min(y), min(z))
-print("Max =", max(x), max(y), max(z))
+    print(xdata, ydata, zdata)
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
 
-cell_ids = {}
-for vert in scaled_verts:
-    indices = [vert[i] // resolution for i in range(3)]
-    id = indices[0] + indices[1] * resolution + indices[2] * resolution**2
-    if id in cell_ids:
-        cell_ids[id] += 1
-    else:
-        cell_ids[id] = 1
+    ax.plot_trisurf(xdata, ydata, zdata, cmap='viridis', edgecolor='none')
+    fig.show()
+    input()
 
-print(len(cell_ids.items()))
-print("Cell IDs min,max =",min(cell_ids), max(cell_ids))
-import operator
-print("Most common IDs: ", sorted(cell_ids.items(), key=operator.itemgetter(1), reverse=True)[:10])
-print("Total vertices found =", sum(cell_ids.values()))
 
-#float x_range = resolution * resolution;
-#float y_range = resolution * 4;
-x_range = resolution ** 2
-y_range = resolution * 4
 
-output_pix_pos = []
-output_pos = []
-#gl_Position = vec4( (2.0 * mod(cell_id[0], x_range) - x_range)/x_range, -1.0 * (2.0 * 4.0 * trunc(cell_id[0] / (resolution * resolution)) - y_range) / y_range , 0.0, 0.0);
-for id in cell_ids.keys():
-    x = id % x_range
-    y =  4 * id // x_range
-    output_pix_pos.append((x, y))
-
-    xpos = (2.0 *  x - x_range)/x_range
-    ypos = -1.0 * (2.0 * y - y_range) / y_range 
-    output_pos.append((xpos, ypos))
-
-x, y = zip(*output_pix_pos)
-print("Output_pix_pos : min, max =",min(x), min(y), "|",max(x),max(y))
-
-print(output_pos[:5])
-#print(len(output_pos))
-x, y = zip(*output_pos)
-print("Output_pos : min, max =",min(x), min(y), "|",max(x),max(y))
-'''
+if __name__ == "__main__":
+    test_cluster_cell_id_gen()
