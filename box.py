f61df559aaa12691b18dd1ee86c3930a38e710be
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2020-11-11 20:33:03 -0600

    Removed old mesh from box.py
    Changed source a while back; not sure what current one is

--- box.py
+++ box.py
@@ -1,4 +1,5 @@
-#From http://web.cse.ohio-state.edu/~crawfis.3/cse581/Slides/cse581_extra_Modeling.pdf
+#Originally from http://web.cse.ohio-state.edu/~crawfis.3/cse581/Slides/cse581_extra_Modeling.pdf
+#  Now unsure (changed source due to bugs (that weren't the mesh's fault))
 
 class Box:
 
@@ -6,21 +7,9 @@ class Box:
         [0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0],
         [0, 1, 1], [1, 1, 1], [1, 0, 1], [0, 0, 1]
     ] 
-    '''
-    [ 1,-1, 1], [ 1,-1,-1], [ 1, 1,-1], [ 1, 1, 1],
-    [-1,-1, 1], [-1,-1,-1], [-1, 1,-1], [-1, 1, 1]
-    '''
     indicies = [
         [0, 1, 2], [0, 2, 3], [2, 4, 3],
 		[2, 5, 4], [1, 5, 2], [1, 6, 5],
 		[0, 4, 7], [0, 3, 4], [5, 7, 4], 
 		[5, 6, 7], [0, 7, 6], [0, 6, 1]
     ]
-    '''
-    [4, 0, 3], [4, 3, 7],
-    [0, 1, 2], [0, 2, 3],
-    [1, 5, 6], [1, 6, 2],
-    [5, 4, 7], [5, 7, 6],
-    [7, 3, 2], [7, 2, 6],
-    [0, 5, 1], [0, 4, 5]
-    '''
